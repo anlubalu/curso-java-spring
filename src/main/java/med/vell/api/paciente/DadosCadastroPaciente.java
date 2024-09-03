65adf7f6f76d476ea59371695b424792ac65cae3
Erick <erickbarretodasilva@gmail.com>
2024-09-03 02:40:32 -0300

    começo do projeto do curso de java spring da alura

---
+++ src/main/java/med/vell/api/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,11 @@
+package med.vell.api.paciente;
+
+import med.vell.api.endereco.DadosEndereco;
+
+public record DadosCadastroPaciente(
+        String nome,
+        String email,
+        String telefone,
+        String cpf,
+        DadosEndereco endereco) {
+}

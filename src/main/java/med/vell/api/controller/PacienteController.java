5a448cc322b01692b479d3ee1f2fc492f95da228
Erick <erickbarretodasilva@gmail.com>
2024-09-05 11:37:50 -0300

    paginação do paciente

--- src/main/java/med/vell/api/controller/PacienteController.java
+++ src/main/java/med/vell/api/controller/PacienteController.java
@@ -3,13 +3,14 @@ package med.vell.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.vell.api.paciente.DadosCadastroPaciente;
+import med.vell.api.paciente.DadosListagemPaciente;
 import med.vell.api.paciente.Paciente;
 import med.vell.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
@@ -23,4 +24,10 @@ public class PacienteController {
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
         repository.save(new Paciente(dados));
     }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
+    }
+
 }

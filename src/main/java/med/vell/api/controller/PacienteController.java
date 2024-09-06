dca0ea567f0114fe7ce1c3917d0b8e26e674ef6e
Erick <erickbarretodasilva@gmail.com>
2024-09-06 00:30:45 -0300

    update e delete do paciente, e também alteração na listagem.

--- src/main/java/med/vell/api/controller/PacienteController.java
+++ src/main/java/med/vell/api/controller/PacienteController.java
@@ -2,10 +2,7 @@ package med.vell.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.vell.api.paciente.DadosCadastroPaciente;
-import med.vell.api.paciente.DadosListagemPaciente;
-import med.vell.api.paciente.Paciente;
-import med.vell.api.paciente.PacienteRepository;
+import med.vell.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -25,9 +22,27 @@ public class PacienteController {
         repository.save(new Paciente(dados));
     }
 
+    //@GetMapping
+    //public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao) {
+        //return repository.findAll(paginacao).map(DadosListagemPaciente::new);
+    //}
     @GetMapping
-    public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
+    public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = { "nome" }) Pageable paginacao) {
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+        var paciente = repository.getReferenceById(dados.id());
+        paciente.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void remover(@PathVariable Long id) {
+        var paciente = repository.getReferenceById(id);
+        paciente.inativar();
     }
 
 }
---
+++ src/main/java/med/vell/api/paciente/DadosAtualizacaoPaciente.java
@@ -0,0 +1,12 @@
+package med.vell.api.paciente;
+
+import jakarta.validation.Valid;
+import med.vell.api.endereco.DadosEndereco;
+
+public record DadosAtualizacaoPaciente(
+        Long id,
+        String nome,
+        String telefone,
+        @Valid DadosEndereco endereco
+) {
+}

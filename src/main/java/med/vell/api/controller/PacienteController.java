9b489bb25987c01379b2c9e213e3f2974a4eeb1b
Erick <erickbarretodasilva@gmail.com>
2024-09-13 10:16:42 -0300

    as respostas agora usam response entity para paciente também

--- src/main/java/med/vell/api/controller/PacienteController.java
+++ src/main/java/med/vell/api/controller/PacienteController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("pacientes")
@@ -18,31 +20,52 @@ public class PacienteController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroPaciente dados, UriComponentsBuilder uriBuilder) {
+        var paciente = new Paciente(dados);
         repository.save(new Paciente(dados));
+
+        // Importa o uri componente para montar uma url no cabeçalho da resposta http
+        var uri = uriBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoPaciente(paciente));
     }
 
     //@GetMapping
     //public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao) {
         //return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     //}
+
+    // Esse código em Java Spring recebe uma configuração de paginação, busca pacientes ativos no banco de dados, mapeia os resultados para um objeto específico e retorna uma página desses objetos em uma resposta HTTP.
     @GetMapping
-    public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = { "nome" }) Pageable paginacao) {
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+    public ResponseEntity<Page<DadosListagemPaciente>> listar(@PageableDefault(page = 0, size = 10, sort = { "nome" }) Pageable paginacao) {
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
         var paciente = repository.getReferenceById(dados.id());
         paciente.atualizarInformacoes(dados);
+
+        // Essa linha constrói e retorna uma resposta HTTP com status 200 OK, contendo um novo objeto DadosDetalhamentoPaciente criado a partir do objeto paciente.
+        return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void remover(@PathVariable Long id) {
+    public ResponseEntity remover(@PathVariable Long id) {
         var paciente = repository.getReferenceById(id);
         paciente.inativar();
+
+        // Essa linha constrói e retorna uma resposta HTTP com status 204 No Content, indicando que a solicitação foi bem-sucedida, mas não há conteúdo a ser retornado.
+        return ResponseEntity.noContent().build();
+    }
+
+    // O response entity retorna uma resposta http adequada. Retornando um DTO para essa requisição.
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var paciente = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
     }
 
 }
---
+++ src/main/java/med/vell/api/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,13 @@
+package med.vell.api.paciente;
+
+import med.vell.api.endereco.Endereco;
+
+// O Record aplica os getters e setters para a classe. Podendo se fosse o caso aplicar as anotações para cada campo para validações.
+public record DadosDetalhamentoPaciente(String nome, String email, String telefone, String cpf, Endereco endereco) {
+
+    // Um construtor que recebe a entidade paciente e recebe somente os getters
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
+    }
+
+}

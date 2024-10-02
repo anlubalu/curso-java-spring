033281811a63d1086387e81d3f495c93facd533f
Erick <erickbarretodasilva@gmail.com>
2024-10-02 07:40:17 -0300

    feat(médico): alteração
    alteração da query
    
    Author: Erick

--- src/main/java/med/vell/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/vell/api/domain/medico/MedicoRepository.java
@@ -15,18 +15,20 @@ public interface MedicoRepository extends JpaRepository<Medico,Long> {
 
     @Query("""
             select m from Medico m
-            where
-            m.ativo = true
-            and
-            m.especialidade = :especialidade
-            and
-            m.id not in(
-                select c.medico.id from Consulta c
-                where
-                c.data = :data
-            )
-            order by rand()
-            limit 1
+                    where
+                    m.ativo = true
+                    and
+                    m.especialidade = :especialidade
+                    and
+                    m.id not in(
+                        select c.medico.id from Consulta c
+                        where
+                        c.data = :data
+                and
+                        c.motivoCancelamento is null
+                    )
+                    order by rand()
+                    limit 1
             """)
     Medico escolherMedicoAleatorioNaData(Especialidade especialidade, LocalDateTime data);
 
